fb36244972eb47634632c64c39cb805401653eda
Kurt Krueckeberg <kurtk@pobox.com>
2013-06-05 15:17:50 -0500

    worked on split

--- tree234.h
+++ tree234.h
@@ -1,6 +1,5 @@
 #ifndef  TREE23_H
 #define	TREE23_H
-// TODO: insertItem() is differs slightly from java source. Also, connectChild() is missing. 
 /*
  * Based on http://www.unf.edu/~broggio/cop3540/Chapter%2010%20-%202-3-4%20Trees%20-%20Part%201.ppt
  * See also: http://grail.cba.csuohio.edu/~lin/cis506/Chapt10.pdf
@@ -41,11 +40,50 @@ template<typename K> class Node234 {
        bool isFull();
        Node234<K> *getParent();
        bool isLeaf(); 
+       void connectChild(int childNum, Node234<K> *child);
 };  
 
-int Node234<K>::MAX = 3;
+template<typename K> int Node234<K>::MAX = 3;
+
+template<typename K> inline Node234<K>::Node234(K small) : totalItems(1)
+{ 
+   keys[0] = small; 
+
+   for (int i = 0; i < MAX; i++) {		
+       children[i] = 0;
+    }
+}
+
+template<typename K> inline Node234<K>::Node234(K small, K middle) : totalItems(2)
+{ 
+   keys[0] = small; 
+   keys[1] = middle; 
+
+   for (int i = 0; i < MAX; i++) {		
+       children[i] = 0;
+    }
+}
+template<typename K> inline Node234<K>::Node234(K small, K middle, K large) : totalItems(3)
+{ 
+   keys[0] = small; 
+   keys[1] = middle; 
+   keys[3] = large; 
+    
+   for (int i = 0; i < MAX; i++) {		
+       children[i] = 0;
+    }
+}
 /*
- * preconditions: node is not full, not a four node (full), and key is not already in node.
+ * precondition: childNum is with the range for the type of node.
+ * child is not 0.
+ */
+template<typename K> inline int Node234<K>::connectChild(int childNum, Node234<K> *child)
+{
+  children[childNum] = child;
+  child->parent = this;
+}
+/*
+ * preconditions: node is not full, not a four node (full), and key is not already in node. It may or may not be a leaf.
  * shifts keys in node as needed so that key will be inserted in sorted position
  */
 template<typename K> inline int Node234<K>::insertItem(K key)
@@ -60,7 +98,7 @@ template<typename K> inline int Node234<K>::insertItem(K key)
             continue;
         } else if (key < keys[i]) { // if it's bigger  
 */
-        if(key < keys[i]) { // if it's bigger
+        if (key < keys[i]) { // if it's bigger
 
             keys[i + 1] = keys[i]; // shift keys[i] right
 
@@ -79,8 +117,8 @@ template<typename K> inline int Node234<K>::insertItem(K key)
     return 0;
 
   /*
-   * TODO: shift pointers appropriately?
-   * A: I believe we are inserting into a leaf, so that is not needed?
+   * TODO: shift pointers appropriately? This seems to be handled separately by the calling code.
+   * A: Also, I believe we are inserting into a leaf, so that is not needed?
    */
 }
 
@@ -98,45 +136,15 @@ template<typename K> inline  bool Node234<K>::isLeaf()
    return !children[0] ? true : false;
 }
 
-template<typename K> inline Node234<K>::Node234(K small) : totalItems(1)
-{ 
-   keys[0] = small; 
-
-   // We only really need to set children[0] to indicated that a node is a leaf, but we set all the children to zero.
-   for (int i = 0; i < MAX; i++) {		
-       children[i] = 0;
-    }
-}
-
-template<typename K> inline Node234<K>::Node234(K small, K middle) : totalItems(2)
-{ 
-   keys[0] = small; 
-   keys[1] = middle; 
-
-   // We only really need to set children[0] to indicated that a node is a leaf, but we set all the children to zero.
-   for (int i = 0; i < MAX; i++) {		
-       children[i] = 0;
-    }
-}
-template<typename K> inline Node234<K>::Node234(K small, K middle, K large) : totalItems(3)
-{ 
-   keys[0] = small; 
-   keys[1] = middle; 
-   keys[3] = large; 
-   // We only really need to set children[0] to indicated that a node is a leaf, but we set all the children to zero.
-   for (int i = 0; i < MAX; i++) {		
-       children[i] = 0;
-    }
-}
-
 template<typename K> inline bool Node234<K>::find(K key, int& index)
 { 
    for(int i =0; i < totalItems; i++) {
    
-           if (keys[i] == key) {
-                   index = i;
-                   return true;
-           }
+       if (keys[i] == key) {
+
+            index = i;
+            return true;
+       }
    }
    return false;
 }
@@ -189,24 +197,24 @@ template<typename K>  bool Tree234<K>::DoSearch(K key, Node234<K> *&location, in
 
   while(true) {
  
-        if (current->find(key, index)) {
+      if (current->find(key, index)) {
 
-            location = current;
-            return true; 
+          location = current;
+          return true; 
 
-        } else if (current->isLeaf()) {
+      } else if (current->isLeaf()) {
 
-            return false;
+          return false;
 
-        } else {
+      } else {
 
-	    current = getNextChild(current, key);	
-        }  
+          current = getNextChild(current, key);	
+      }  
     }
 }
 /* 
  * Precondition: assumes node is not empty, not full, not a leaf
- * Returns next child
+ * Returns next child (there could be up to three children)
  */
 template<typename K> inline Node234<K> *Tree234<K>::getNextChild(Node234<K> *current, K key)
 {
@@ -239,42 +247,42 @@ template<typename K> template<typename Functor> void Tree234<K>::DoTraverse(Func
 
    switch (current->totalItems) {
 
-        case 1: // two node
-                 DoTraverse(f, current->children[0]);
+      case 1: // two node
+            DoTraverse(f, current->children[0]);
 
-                 f(current->keys[0]);
+            f(current->keys[0]);
 
-                 DoTraverse(f, current->children[1]);
-                 break;
+            DoTraverse(f, current->children[1]);
+            break;
 
-        case 2: // three node
-                 DoTraverse(f, current->children[0]);
+      case 2: // three node
+            DoTraverse(f, current->children[0]);
 
-                 f(current->keys[0]);
+            f(current->keys[0]);
 
-                 DoTraverse(f, current->children[1]);
+            DoTraverse(f, current->children[1]);
  
-                 f(current->keys[1]);
+            f(current->keys[1]);
 
-                 DoTraverse(f, current->children[2]);
-                 break;
+            DoTraverse(f, current->children[2]);
+            break;
 
-        case 3: // four node
-                 DoTraverse(f, current->children[0]);
+      case 3: // four node
+            DoTraverse(f, current->children[0]);
 
-                 f(current->keys[0]);
+            f(current->keys[0]);
 
-                 DoTraverse(f, current->children[1]);
+            DoTraverse(f, current->children[1]);
  
-                 f(current->keys[1]);
+            f(current->keys[1]);
 
-                 DoTraverse(f, current->children[2]);
+            DoTraverse(f, current->children[2]);
 
-                 f(current->keys[2]);
+            f(current->keys[2]);
 
-                 DoTraverse(f, current->children[3]);
+            DoTraverse(f, current->children[3]);
  
-                 break;
+            break;
    }
 }
 
@@ -310,28 +318,38 @@ Node234<K> *current = root;
     // current is now a leaf and not full.
     current->insertItem(key); 
 } 
-/*
+/* split(Node234<K> *nod)
  * Preconditions: node is full, a four node.
  *
- * Pseudo code: The four node can be either: 
- * 1. root
- * 2. have a two node parent
- * 3. have a three node parent
+ * Pseudo code
+ * The four node is either: 
+ * 1. the root
+ * 2. has a two node parent
+ * 3. has a three node parent
  * 
  * If (node is root) { 
  *   put middle value in a new parent two node.
  *   save the large value temporarily.
- *   convert node into a two node, holding the samll value
+ *   convert node into a two node, holding the small value, use the left most child as its left child, set its next left most child as its right child.
  *   allocate a new two node to hold the large value. 
- *   set the node as left child of new parent. set the newly allocated node as the right child
+ *   set the converted node as left child of new parent. set the other allocated node as the right child
  *   return 
  * } 
+ *
  * parent = parent of node
  *
  * if (parent is two node) {
  *
+ *   if (parent's left child == node) {
+ *           
  *
- * } else { // parent is three node
+ *
+ *   } else { // node is right child
+ *
+ *
+ *
+ *   }
+ * } else { // parent is three node, and node is either left, middle or right child.
  *
  *
  *
@@ -340,23 +358,120 @@ Node234<K> *current = root;
  */
 template<typename K> void split(Node234<K> *node)
 {
+    K  itemB, itemC;
+
+    Node234<K> *parent, *child2, *child3;
+
+    int itemIndex;
+
+    // remove two largest (of three total) keys 
+    /*
+    itemC = node.removeItem(); 
+    
+    itemB = node.removeItem(); 
+    */
+
+    itemC = node->keys[2];
+    itemB = node->keys[1]; 
+
+    // Remove two right-most children from this node. 
+    /*
+    child2 = node.disconnectChild(2); 
+    
+    child3 = node.disconnectChild(3);
+    */
+    child2 = node->children[2]; 
+    child3 = node->children[3]; 
+
+    node->children[2] = 0; 
+    node->children[3] = 0; 
+    
+    Node newRight = new Node(itemC); // make new right child node
+
+    /* set its left and right children to be the two right most children of node */
+    newRight->connectChild(0, child2); // connect to 0 and 1
+
+    newRight->connectChild(1, child3); // on newRight
+
+    // if this is the root,
+    if(node == root) { 
+        /* make new root two node using node's middle value */  
+	root = new Node(itemB); 
+	parent = root;          // root is parent of node
+	root->connectChild(0, node); // connect node to root as left child
+        root->connectChild(1, newRight);
+        return;
+
+    } /*else {    
+        // node is not the root, get parent 
+	parent = node->getParent(); 
+    } */
+    
+    parent = node->getParent(); 
+    bool bParentWas2Node = parent->totalItems == 1;
+
+    // deal with parent, moving itemB middle value to parent.
+
+    int insertIndex = parent->insertItem(itemB);
+
+    if (bParentWas2Node) { // parent was two node, now is three node
+
+        // move parent's connections
+
+    } else { // parent was three node, now is a four node
+
+        // move parent's connections
+
+    }
+
+    return;
+
+    itemIndex = parent->insertItem(itemB); // item B to parent
+    
+    int total = parent->totalItems; // total items?
+    
+    for(int j=total - 1; j > itemIndex; j--)  {// move parent's connections
+
+        Node234<K> *temp = parent->disconnectChild(j); // one child
+
+        parent->connectChild(j + 1, temp); // to the right
+    }
+
+    // connect newRight to parent
+    parent->connectChild(itemIndex + 1, newRight);
+
+    // deal with newRight
+    /*
+    newRight->insertItem(itemC); // item C to newRight
+
+    newRight->connectChild(0, child2); // connect to 0 and 1
+
+    newRight->connectChild(1, child3); // on newRight
+    */
+}
+
+template<typename K> void splitjava(Node234<K> *node)
+{
     K  temB, itemC;
 
     Node234<K> *parent, *child2, *child3;
 
     int itemIndex;
-    
-    itemC = node.removeItem(); // remove items from node
+
+    // remove two largest (of three total) keys 
+    itemC = node.removeItem(); 
     
     itemB = node.removeItem(); 
+
+    // Remove two right-most children from this node. 
+    child2 = node.disconnectChild(2); 
     
-    child2 = node.disconnectChild(2); // remove children
-    
-    child3 = node.disconnectChild(3); // from this node
+    child3 = node.disconnectChild(3);
     
     Node newRight = new Node(); // make new node
 
-    if(node == root) { // if this is the root,
+    // if this is the root,
+    if(node == root) { 
 
 	root = new Node(); // make new root
 	parent = root;     // root is our parent
