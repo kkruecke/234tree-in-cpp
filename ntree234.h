f8910719651ca4e970cfe9937a726be5c5c75fec
Kurt Krueckeberg <kurtk@pobox.com>
2014-10-10 11:54:21 -0500

    add noexcept

--- ntree234.h
+++ ntree234.h
@@ -28,10 +28,10 @@ template<typename K> class Tree234 {
        friend class DebugPrinter;
        static int MAX_KEYS;   
 
-       Node234(K small);
-       Node234(K small, K larger);
-       Node234(K small, K middle, K large);
-       Node234& operator=(const Node234& rhs);
+       Node234(K small) noexcept;
+       Node234(K small, K larger) noexcept;
+       Node234(K small, K middle, K large) noexcept;
+       Node234& operator=(const Node234& rhs) noexcept;
 
        Node234 *parent; // parent is only used for navigation of the tree. It does not own the memory it points to.
 
@@ -47,24 +47,24 @@ template<typename K> class Tree234 {
 
        std::array< std::unique_ptr<Node234>, 4 > children;
 
-       Node234 *getParent(); 
+       Node234 *getParent() noexcept; 
 
        /* 
         * Returns true if key is found in node and set index: this->keys[index] == key
         * Returns false if key is if not found; set next to the next in-order child.
         */
-       bool searchNode(K key, int& index, Node234 *&next);
+       bool searchNode(K key, int& index, Node234 *&next) noexcept;
 
-       int insertKey(K key);
+       int insertKey(K key) noexcept;
        
-       void connectChild(int childNum, std::unique_ptr<Node234>& child);
+       void connectChild(int childNum, std::unique_ptr<Node234>& child) noexcept;
        
        /*
         * Removes child node andshifts its children to fill the gap. Returns child pointer.
         */  
-       std::unique_ptr<Node234> disconnectChild(int child_index); 
+       std::unique_ptr<Node234> disconnectChild(int child_index) noexcept; 
 
-       void insertChild(int childNum, std::unique_ptr<Node234> &pChild);
+       void insertChild(int childNum, std::unique_ptr<Node234> &pChild) noexcept;
 
        /* 
         * Called during remove(K key).
@@ -73,37 +73,37 @@ template<typename K> class Tree234 {
        
      public:
          
-       const Node234 *getParent() const;
+       const Node234 *getParent() const noexcept;
 
-       int getTotalItems() const;
-       int getChildCount() const;
-       bool findKey(K key, int& index) const;
-       bool isFull() const;
-       bool isLeaf() const; 
-       bool isTwoNode() const;
-       void nullAllChildren();
+       int getTotalItems() const noexcept;
+       int getChildCount() const noexcept;
+       bool findKey(K key, int& index) const noexcept;
+       bool isFull() const  noexcept;
+       bool isLeaf() const noexcept; 
+       bool isTwoNode() const noexcept;
+       void nullAllChildren() noexcept;
     };  
 
     friend class DebugPrinter;
   
     std::unique_ptr<Node234>  root; 
     
-    bool DoSearch(K key, Node234 *&location, int& index);
+    bool DoSearch(K key, Node234 *&location, int& index) noexcept;
 
-    void split(Node234 *node);  // called during insert to split 4-nodes
-    void DestroyTree(std::unique_ptr<Node234> &root); 
+    void split(Node234 *node) noexcept;  // called during insert to split 4-nodes
+    void DestroyTree(std::unique_ptr<Node234> &root) noexcept; 
 
   public:
 
-     Tree234() : root{nullptr} { } 
+     Tree234() noexcept : root{nullptr} { } 
 
-     Tree234(const Tree234& lhs); 
+     Tree234(const Tree234& lhs) noexcept; 
 
-     Tree234(std::initializer_list<K> list); 
+     Tree234(std::initializer_list<K> list) noexcept; 
 
     ~Tree234(); 
 
-    bool search(K key);
+    bool search(K key) noexcept;
 
     void insert(K key) noexcept; 
 };
@@ -118,7 +118,7 @@ template<typename K> int  Tree234<K>::Node234::MAX_KEYS = 3;
  * TODO: 
  * error: non-const lvalue reference to type 'std::unique_ptr<Node234>' cannot bind to a temporary of type 'nullptr_t'
  */
-template<typename K> inline  Tree234<K>::Node234::Node234(K small) : totalItems(1), parent(nullptr)
+template<typename K> inline  Tree234<K>::Node234::Node234(K small)  noexcept : totalItems(1), parent(nullptr)
 { 
    keys[0] = small; 
    children[0] = nullptr;
@@ -129,7 +129,7 @@ template<typename K> inline  Tree234<K>::Node234::Node234(K small) : totalItems(
  * error: non-const lvalue reference to type 'std::unique_ptr<Node234>' cannot bind to a temporary of type 'nullptr_t'
  */
 
-template<typename K> inline  Tree234<K>::Node234::Node234(K small, K middle) : totalItems(2), parent(nullptr)
+template<typename K> inline  Tree234<K>::Node234::Node234(K small, K middle)  noexcept : totalItems(2), parent(nullptr)
 { 
    keys[0] = small; 
    keys[1] = middle; 
@@ -139,7 +139,7 @@ template<typename K> inline  Tree234<K>::Node234::Node234(K small, K middle) : t
  * TODO: 
  * error: non-const lvalue reference to type 'std::unique_ptr<Node234>' cannot bind to a temporary of type 'nullptr_t'
  */
-template<typename K> inline  Tree234<K>::Node234::Node234(K small, K middle, K large) : totalItems(3), parent(nullptr)
+template<typename K> inline  Tree234<K>::Node234::Node234(K small, K middle, K large)  noexcept : totalItems(3), parent(nullptr)
 { 
    keys[0] = small; 
    keys[1] = middle; 
@@ -147,7 +147,7 @@ template<typename K> inline  Tree234<K>::Node234::Node234(K small, K middle, K l
    children[0] = nullptr;
 }
 
-template<typename K> inline void Tree234<K>::Node234::nullAllChildren()
+template<typename K> inline void Tree234<K>::Node234::nullAllChildren()  noexcept
 {
   for(auto i = 0; i <= totalItems; ++i) {
 
@@ -155,7 +155,7 @@ template<typename K> inline void Tree234<K>::Node234::nullAllChildren()
   }
 }
 
-template<typename K> typename Tree234<K>::Node234& Tree234<K>::Node234::operator=(const Node234& rhs)
+template<typename K> typename Tree234<K>::Node234& Tree234<K>::Node234::operator=(const Node234& rhs)  noexcept
 {
   if (this == &rhs) {
     return rhs;
@@ -173,12 +173,12 @@ template<typename K> typename Tree234<K>::Node234& Tree234<K>::Node234::operator
   children[i] = rhs.children[i];
 }
 
-template<typename K> inline int Tree234<K>::Node234::getTotalItems() const
+template<typename K> inline int Tree234<K>::Node234::getTotalItems() const noexcept
 {
    return totalItems; 
 }
 
-template<typename K> inline bool Tree234<K>::Node234::findKey(K key, int& index) const
+template<typename K> inline bool Tree234<K>::Node234::findKey(K key, int& index) const noexcept
 {
    for(index = 0; index < totalItems; ++index) {
        
@@ -190,18 +190,18 @@ template<typename K> inline bool Tree234<K>::Node234::findKey(K key, int& index)
    return false;
 }
 
-template<typename K> inline int Tree234<K>::Node234::getChildCount() const
+template<typename K> inline int Tree234<K>::Node234::getChildCount() const noexcept
 {
    return totalItems + 1; 
 }
 
-template<typename K> inline bool Tree234<K>::Node234::isTwoNode() const
+template<typename K> inline bool Tree234<K>::Node234::isTwoNode() const noexcept
 {
    return (totalItems == 1) ? true : false;
 }
 
  
-template<typename K> inline Tree234<K>::Tree234(std::initializer_list<K> il) //: root(nullptr)
+template<typename K> inline Tree234<K>::Tree234(std::initializer_list<K> il) noexcept//: root(nullptr)
 {
     for (auto x: il) {
 
@@ -212,7 +212,7 @@ template<typename K> inline Tree234<K>::Tree234(std::initializer_list<K> il) //:
  * precondition: childIndex is within the range for the type of node.
  * child is not nullptr.
  */
-template<typename K> inline void  Tree234<K>::Node234::connectChild(int childIndex, std::unique_ptr<Node234>& child)
+template<typename K> inline void  Tree234<K>::Node234::connectChild(int childIndex, std::unique_ptr<Node234>& child)  noexcept
 {
   children[childIndex] = std::move( child );
   
@@ -226,7 +226,7 @@ template<typename K> inline void  Tree234<K>::Node234::connectChild(int childInd
  * Returns true if key is found in node, and it set index so that this->keys[index] == key.
  * Returns false if key is if not found, and it sets next to point to next child with which to continue the search.
  */
-template<typename K> inline bool Tree234<K>::Node234::searchNode(K value, int& index, Node234 *&next)
+template<typename K> inline bool Tree234<K>::Node234::searchNode(K value, int& index, Node234 *&next) noexcept
 {
  bool hit = false;
 
@@ -252,7 +252,7 @@ template<typename K> inline bool Tree234<K>::Node234::searchNode(K value, int& i
   return hit;
 }
 
-template<typename K> inline void Tree234<K>::Node234::insertChild(int childNum, std::unique_ptr<Node234> &pChild)
+template<typename K> inline void Tree234<K>::Node234::insertChild(int childNum, std::unique_ptr<Node234> &pChild) noexcept
 {
   // shift children right in order to insert pChild
   
@@ -285,7 +285,7 @@ template<typename K> inline void Tree234<K>::Node234::insertChild(int childNum, 
  * will have been altered.
  */
 
-template<typename K> inline std::unique_ptr<typename Tree234<K>::Node234> Tree234<K>::Node234::disconnectChild(int childIndex)
+template<typename K> inline std::unique_ptr<typename Tree234<K>::Node234> Tree234<K>::Node234::disconnectChild(int childIndex) noexcept
 {
   std::unique_ptr<Node234> node{ children[childIndex] }; // invokes move constructor.
 
@@ -303,7 +303,7 @@ template<typename K> inline std::unique_ptr<typename Tree234<K>::Node234> Tree23
  * of inserted key.
  */
 
-template<typename K> inline int  Tree234<K>::Node234::insertKey(K key) 
+template<typename K> inline int  Tree234<K>::Node234::insertKey(K key)  noexcept
 { 
   // start on right, examine items
 
@@ -329,23 +329,23 @@ template<typename K> inline int  Tree234<K>::Node234::insertKey(K key)
 }
 
 
-template<typename K> inline  bool Tree234<K>::Node234::isFull() const 
+template<typename K> inline  bool Tree234<K>::Node234::isFull() const  noexcept
 { 
    return totalItems == MAX_KEYS;
 }
 
-template<typename K> inline typename Tree234<K>::Node234 * Tree234<K>::Node234::getParent()  
+template<typename K> inline typename Tree234<K>::Node234 * Tree234<K>::Node234::getParent()   noexcept
 { 
    return parent;
 }
 
-template<typename K> inline const typename Tree234<K>::Node234 *Tree234<K>::Node234::getParent() const 
+template<typename K> inline const typename Tree234<K>::Node234 *Tree234<K>::Node234::getParent() const  noexcept
 { 
    return parent;
 }
 
 
-template<typename K> inline  bool Tree234<K>::Node234::isLeaf() const 
+template<typename K> inline  bool Tree234<K>::Node234::isLeaf() const  noexcept
 { 
    return !children[0] ? true : false;
 }
@@ -355,7 +355,7 @@ template<typename K> inline Tree234<K>::~Tree234()
   // DestroyTree(root); TODO: DestroyTree should call p.release() instead of delete p;
 }
 
-template<typename K> inline bool Tree234<K>::search(K key)
+template<typename K> inline bool Tree234<K>::search(K key) noexcept
 {
     // make sure tree has at least one element    
     if (root == nullptr) {
@@ -369,7 +369,7 @@ template<typename K> inline bool Tree234<K>::search(K key)
     }
 }   
 
-template<typename K>  bool Tree234<K>::DoSearch(K key, Node234 *&location, int& index)
+template<typename K>  bool Tree234<K>::DoSearch(K key, Node234 *&location, int& index) noexcept
 {
   Node234 *current = root.get();
   Node234 *next;
@@ -462,7 +462,7 @@ template<typename K> void Tree234<K>::insert(K key) noexcept
  * 2. has a two node parent
  * 3. has a three node parent
  */ 
-template<typename K> void Tree234<K>::split(Node234 *node)
+template<typename K> void Tree234<K>::split(Node234 *node) noexcept
 {
     K  itemB, itemC;
     Node234 *parent;
@@ -498,7 +498,8 @@ template<typename K> void Tree234<K>::split(Node234 *node)
         
         root = std::move(p); // TODO: Is this what I intend?
         
-        parent = root;          // root is parent of node
+        // TODO: Do this line do anything?
+        parent = root;          // root is parent of node 
         
         root->connectChild(0, node); // connect node to root as left child
         
