bdaa35405df952ad63d3a1c27e809b5fa029c191
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2017-09-03 20:08:15 -0400

    updated code with new prospective version of getLeafNodeSuccessor(). See comments in code.

--- include/tree234-new.h
+++ include/tree234-new.h
@@ -1,6 +1,6 @@
 #ifndef TREE234_H
 #define	TREE234_H
-
+::
 #include <utility>
 #include <algorithm>
 #include <stdexcept>
@@ -825,6 +825,35 @@ template<class Key, class Value> std::pair<const typename tree234<Key, Value>::N
  */
 template<class Key, class Value> std::pair<const typename tree234<Key, Value>::Node234 *, int> tree234<Key, Value>::getLeafNodeSuccessor(const typename tree234<Key, Value>::Node234 *pnode, int index_of_key) const noexcept
 {
+	// New Version start
+	
+	 // 
+  if (!pnode->isTwoNode() && (pnode->getTotalItems() - 1) != index_of_key) { // pnode is a 3 or 4-node and the index is not the right most.
+
+      return std::make_pair(pnode, index_of_key + 1); 
+  }
+
+  /*
+   If pnode is a 3- or 4-node, key_index is the right-most; or if pnode is a 2-node, it is by default he "right most"
+   If child_index is the right most index (of the parent node), then we must ascent the tree until we encounter the first ancestor that is not a
+   right-most child; otherwise, ....
+   */
+   if (child_index == pnode->parent->getRightMostChildIndex()) {
+
+
+       while (??? != right-most) {
+
+       }
+
+
+   } else { // what do we do?
+
+
+
+   }
+	// New Version end
+	
+	
   // ported  
   if (!pnode->isTwoNode() && (pnode->getTotalItems() - 1) != index_of_key) { // pnode is a 3 or 4-node and the index is not the right most.
 
