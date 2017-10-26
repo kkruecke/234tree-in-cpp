baa41d364e3450f0505f701355f52b38a26d6ad1
kurt krueckeberm <kurt.krueckeberg@gmail.com>
2017-10-26 10:44:03 -0400

    added new untested method

--- include/tree234.h
+++ include/tree234.h
@@ -213,6 +213,8 @@ template<typename Key, typename Value> class tree234 {
 
     Node *rightRotation(Node *p2node, Node *psibling, Node *parent, int parent_key_index) noexcept;
 
+    const Node *getInternalNodeSuccessor_remove(Key key, const Node *current) noexcept; // new untested code
+
     // Non recursive in order traversal of tree methods
     std::pair<const Node *, int> getSuccessor(const Node *current, int key_index) const noexcept;
     std::pair<const Node *, int> getPredecessor(const Node *current, int key_index) const noexcept;
@@ -1628,6 +1630,8 @@ template<typename Key, typename Value> bool tree234<Key, Value>::remove(Key key,
       // found_node->children[found_index + 1].
       current = pfound_node->children[key_index + 1].get(); 
 
+      //++ getInternalNodeSuccessor_remove(key, current); replaces all code within this if-block.
+
       while (true) {
 
         if (current->isTwoNode()) { 
@@ -1642,6 +1646,7 @@ template<typename Key, typename Value> bool tree234<Key, Value>::remove(Key key,
              // pfound_node is not deleted if pfound_node is the root (and the root is a 2-node), and no nodes get deleted when either a
              // rightRotation or leftRotation occurs. So pfound_node is safe then. Finally, pfound_node is not deleted during fuseSiblings().
              // fuseSiblings() deletes a 2-node sibling but not pfound_node itself. 
+            
              if (pfound_node->getTotalItems() - 1 < key_index || pfound_node->keys_values[key_index].key() != key) { // then key moved
 
                  // ...simply recurse, starting with a new initial starting point of pfound_node.
@@ -1678,6 +1683,34 @@ template<typename Key, typename Value> bool tree234<Key, Value>::remove(Key key,
   --tree_size;
 
   return true;
+}
+/*
+ Looks for in order successor while converting any 2-nodes encountered to 3-nodes
+ */
+template<typename Key, typename Value> const typename tree234<Key, Value>::Node *tree234<Key, Value>::getInternalNodeSuccessor_remove(Key key, const Node *current) noexcept
+{
+   int child_index;
+   int key_index;
+   const Node *next = nullptr;
+   
+   while (true) {
+
+      if (current->isTwoNode()) { 
+    
+             current = convertTwoNode(const_cast<Node*>(current));
+      } 
+
+      current->SearchNode(key, key_index, child_index, next);  // ...advance down the tree. We use the side effects on next and child_index to traverse to successor. 
+
+      if (current->isLeaf()) { // Are we done? 
+
+         break; 
+      } 
+
+      child_index = 0; // This must be set inside this loop, as it is used below.
+      current = current->children[child_index].get(); // set current to left most child of the node, 
+  }
+  return current;
 }
 /*
  * Requires: node is 2-node.
