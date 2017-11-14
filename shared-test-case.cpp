6e9f98a28b07df240b0d7293bf1bf35639a54116
kurt krueckeberm <kurt.krueckeberg@gmail.com>
2017-11-14 17:16:38 -0500

    See shared-test-case.cpp

--- shared-test-case.cpp
+++ shared-test-case.cpp
@@ -1,51 +1,21 @@
-#include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <utility>
-using namespace std;
+#include <queue>
+#include <stack>
 
-/*
- Object for testing trees implemented with shared_ptr's
- */
-
-class Test {
-
-   int i;
-
-  public:
-
-   bool operator <(const Test& lhs) { return i < lhs.i; }
-   bool operator==(const Test& lhs) { return i == lhs.i; }
-
-   Test() : i{0} {}
-   Test(int in) : i{in} {}
-   Test(const Test& ) = default;
-
-   Test(Test&& ) = default;
-
-   Test& operator=(const Test&) = default;
-   Test& operator=(Test&&) = default;
-
-   friend ostream& operator<<(ostream& ostr, const Test& lhs) 
-   {
-     ostr << "i = " << lhs.i << endl; 
-     return ostr;
-   }
-
-  ~Test()
-   {
-     std::cout << "In ~Test() with i = " << i << " and this = " << this << std::endl;
-   }
- 
-};
+// TODO: Implement print_tree()
 
 template<class Key> class Node;  // fwd ref.
 
 template<class Key> std::shared_ptr<Node<Key>> buildTree(Key k1, Key k2, Key k3); // fwd ref.
 
+template<class Key> void print_tree(const Node<Key>& root); // fwd ref.
+
 template<class Key> class Node {
 
    friend std::shared_ptr<Node<Key>> buildTree<Key>(Key k1, Key k2, Key k3); 
+   friend void print_tree<Key>(const Node<Key>&); 
 
     Key key;
 
@@ -62,17 +32,18 @@ template<class Key> class Node {
    Node<Key>& operator=(const Node<Key>&) = default;
    Node<Key>& operator=(Node<Key>&&) = default;
    
+   Key get_key() { return key; } 
 
   ~Node()
    {
-     std::cout << "In ~Node<Key> with key = " << key << " and this = " << this << std::endl;
+     std::cout << "In ~Node<Key> with key = \n\t" << get_key() << "\n\t and this = " << this << '.' << std::endl;
    }
  
 };
 
-template<class Key> shared_ptr<Node<Key>> buildTree(Key k1, Key k2, Key k3)
+template<class Key> std::shared_ptr<Node<Key>> buildTree(Key k1, Key k2, Key k3)
 {
-  shared_ptr<Node<Key>> tree;
+  std::shared_ptr<Node<Key>> tree;
 
   Key min, mid, max;
 
@@ -101,22 +72,98 @@ template<class Key> shared_ptr<Node<Key>> buildTree(Key k1, Key k2, Key k3)
      max = k3;
   }
 
-  tree = make_shared<Node<Key>>(mid);   
+  tree = std::make_shared<Node<Key>>(mid);   
 
-  tree->left =  make_shared<Node<Key>>(min);   
+  tree->left =  std::make_shared<Node<Key>>(min);   
 
-  tree->right =  make_shared<Node<Key>>(max);   
+  tree->right =  std::make_shared<Node<Key>>(max);   
 
   return tree;
 }
 
+template<class Key, class Functor> void levelOrderTraverse(const Node<Key>& root, Functor f) 
+{
+   if (root == nullptr) return;
+   
+   // pair of: 1. Node pointer and 2. level of tree.
+   std::queue<std::pair<const Node<Key>*, int>> q; 
+
+   auto level = 1;
+
+   q.push(std::make_pair(root.get(), level));
+
+   while (!q.empty()) {
+
+        std::pair<const Node<Key> *, int> pair_ = q.front();
+
+        const Node<Key> *current = pair_.first;
+
+        int tree_level = pair_.second;
+
+        f(current, tree_level); // For example: print out all the keys_values in current.
+         
+        if (!current->isLeaf()) {
+            
+            q.push(std::make_pair(current->left.get(), tree_level + 1));  
+            q.push(std::make_pair(current->right.get(), tree_level + 1));  
+        }
+        q.pop(); 
+   }
+}
+
+template<class Key> void print_tree(const Node<Key>& root)
+{
+  // auto functor = [&](const Node *current, int level) { 
+
+}
+
+
+/*
+ Object for testing trees implemented with shared_ptr's
+ */
+
+class Test {
+
+   int i;
+
+  public:
+
+   bool operator <(const Test& lhs) { return i < lhs.i; }
+   bool operator==(const Test& lhs) { return i == lhs.i; }
+
+   Test() : i{0} {}
+   Test(int in) : i{in} {}
+   Test(const Test& ) = default;
+
+   Test(Test&& ) = default;
+
+   Test& operator=(const Test&) = default;
+   Test& operator=(Test&&) = default;
+
+   friend std::ostream& operator<<(std::ostream& ostr, const Test& lhs) 
+   {
+     ostr << "i = " << lhs.i << std::endl; 
+     return ostr;
+   }
+
+  ~Test()
+   {
+     std::cout << "In ~Test() with i = " << i << " and this = " << this << std::endl;
+   }
+ 
+};
+
+using namespace std;
 
 int main(int argc, char** argv) 
 {
     shared_ptr<Node<Test>> root = buildTree(Test(0), Test{1}, Test{2});
+
+    cout << "\nTree built." << endl;
    
     shared_ptr<Node<Test>> root2 = root;
     
+    cout << "\nroot2 = root done. Doing 'return 0'." << endl;
     return 0;
 }
 
