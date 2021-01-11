dbc47ca28587d17d738d6a84705b6547a51f0de2
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2021-01-11 07:37:11 -0500

    Update tree234.h

--- include/tree234.h
+++ include/tree234.h
@@ -1667,13 +1667,15 @@ tree234<Key, Value>::get_delete_successor(Node *pdelete, Key delete_key, int del
 
       child_index = convert2Node(rightSubtree, child_index);  
     /*
-      Check if, when we converted the rightSubtree, delete_key moved.  
-      Comments: If the root of the right subtree had to be converted, either a rotation occurred, or a fusion (with the parent, rightSubtree and a
-      sibling occurred). If a left rotation occurred (that "stold" a key from the left sibling and brought down the delete_key), then delete_key
-      becomes the first key rightSubtree. If a right rotation occurred, delete_key is unaffected. This applies regardless whether pdelete is a 3-node
-      or a 4-node.
-      If a fusion of the rightSubtree with a parent key and a sibling key occurred, delete_key becomes the 2nd key in rightSubtree. 
-      Therefore we check if delete_key is now the first or second key of rightSubtree, and...
+      Check if, when we converted the rightSubtree, delete_key moved...
+
+       Comments: If the root of the right subtree had to be converted, either a rotation occurred, or a fusion (with the parent, rightSubtree and a
+       sibling occurred). If a left rotation occurred (that "stole" a key from the left sibling and brought down the delete_key), then delete_key
+       becomes the first key of rightSubtree. If a right rotation occurred, delete_key is unaffected. This applies regardless whether pdelete is a 3-node
+       or a 4-node.
+
+       If a fusion of the rightSubtree with a parent key and a sibling key occurred, delete_key becomes the 2nd key in rightSubtree. 
+       Therefore we check if delete_key is now the first or second key of rightSubtree, and...
      */
      if (delete_key == rightSubtree->key(0) || delete_key == rightSubtree->key(1)) {              
 
